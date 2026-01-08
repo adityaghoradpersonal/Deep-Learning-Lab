d834ed834f326d8ce21dca02dd3d6051789b79c8
adityaghoradpersonal <adityaghoradpersonal@gmail.com>
2026-01-08 15:38:43 +0530

    Lab 1: Implement PCA and generate Scree Plot for variance analysis

--- Lab1-PCA/pca_dimensionality_reduction.py
+++ Lab1-PCA/pca_dimensionality_reduction.py
@@ -25,3 +25,30 @@ X_scaled = scalar.fit_transform(x)
 
 print("\nStandardized Data Preview:")
 print(X_scaled[:5])
+
+pca = PCA(n_components=None)
+
+X_pca = pca.fit_transform(X_scaled)
+
+pca_columns = [f'PC{i+1}' for i in range(X_pca.shape[1])]
+df_pca = pd.DataFrame(data = X_pca, columns = pca_columns)
+
+print("\nPrincipal Components:")
+print(df_pca.head())
+
+# Create the plot
+plt.figure(figsize=(8, 5))
+plt.plot(range(1, 5), pca.explained_variance_ratio_, 'ro-', linewidth=2)
+
+# Adding labels and titles
+plt.title('Scree Plot')
+plt.xlabel('Principal Component')
+plt.ylabel('Proportion of Variance Explained')
+plt.xticks(range(1, 5)) # Ensure x-axis shows 1, 2, 3, 4
+plt.grid(True)
+plt.show()
+
+# Print the exact variance values
+print("\nExplained Variance Ratio per component:")
+for i, ratio in enumerate(pca.explained_variance_ratio_):
+    print(f"PC{i+1}: {ratio:.4f}")
